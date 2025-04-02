7182ae4aff7b3b8452f9a0126e2c68c1c000a5ef
Kone Abdallah Hakim <71593784+schalappe@users.noreply.github.com>
2025-04-03 01:03:42 +0200

    chore: remove Registry

--- reinforce/core/__init__.py
+++ reinforce/core/__init__.py
@@ -7,12 +7,10 @@ from reinforce.core.base_agent import BaseAgent
 from reinforce.core.base_environment import BaseEnvironment
 from reinforce.core.base_evaluator import BaseEvaluator
 from reinforce.core.base_trainer import BaseTrainer
-from reinforce.core.registry import Registry
 
 __all__ = [
     "BaseAgent",
     "BaseEnvironment",
     "BaseTrainer",
     "BaseEvaluator",
-    "Registry",
 ]
